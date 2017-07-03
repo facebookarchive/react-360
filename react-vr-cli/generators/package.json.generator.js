50009857a2756a4a3b7b6814d084627ad86a3432
Mike Armstrong <mike.armstrong@oculus.com>
2017-07-03 10:57:25 -0700

    Package generator had incorrect versions
    Summary: Updating the generator package.json with the updated react and react native versions
    
    Reviewed By: andrewimm
    
    Differential Revision: D5362414
    
    fbshipit-source-id: 789798f

--- react-vr-cli/generators/package.json.generator.js
+++ react-vr-cli/generators/package.json.generator.js
@@ -24,8 +24,8 @@ module.exports = config => ({
   },
   "dependencies": {
     "ovrui": "~1.0.0",
-    "react": "~15.4.1",
-    "react-native": "~0.42.0",
+    "react": "16.0.0-alpha.12",
+    "react-native": "~0.45.0",
     "three": "^0.85.2",
     "react-vr": "~1.0.0",
     "react-vr-web": "~1.0.0"
