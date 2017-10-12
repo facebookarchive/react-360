81a1c7a53df85e0d0576a4c3f9de645e80c8517a
Marcos Carranza <mcarranza@fb.com>
2017-10-12 15:45:01 -0700

    DomOverlaySample - Define window.process instead of process global for react to start correctly on overlay sample.
    Reviewed By: andrewimm
    
    Differential Revision: D6033087
    
    fbshipit-source-id: 49ecd3c

--- Examples/DomOverlaySample/process.js
+++ Examples/DomOverlaySample/process.js
@@ -19,9 +19,12 @@
 // } else {
 //   module.exports = require('./cjs/react.development.js');
 // }
-// So we define it to point to production.
-process = {
+window.process = {
   env: {
     NODE_ENV: 'production',
   },
 };
+
+if (__DEV__) {
+  window.process.env.NODE_ENV = 'development';
+}
