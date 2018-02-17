a19c585c26636098e78a74c093cd29f5698b8249
Andrew Imm <andrewi@fb.com>
2018-02-17 11:59:29 -0800

    Determinator to run legocastle scripts on oss changes
    Reviewed By: mikearmstrong001
    
    Differential Revision: D6993181
    
    fbshipit-source-id: 7191464bfcbe63eb7481ad9cbb6b302e30494835

---
+++ defs/chalk.js
@@ -0,0 +1,6 @@
+declare module 'chalk' {
+  declare function blue(string): string;
+  declare function gray(string): string;
+  declare function green(string): string;
+  declare function red(string): string;
+}
