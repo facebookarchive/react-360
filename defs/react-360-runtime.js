88dd8bb7312ce9212f97a6f0c4410f05d26516af
Yidong Lin <larrylin@oculus.com>
2018-11-02 12:09:09 +0000

    [2018/11/02] publishing latest react-360-web package

--- defs/react-360-runtime.js
+++
@@ -1,7 +0,0 @@
-declare interface SceneGraphNode {
-  children: SceneGraphNode[];
-  parent: ?SceneGraphNode;
-
-  add(...nodes: SceneGraphNode[]): mixed;
-  remove(...nodes: SceneGraphNode[]): mixed;
-}
